94d3c30cbac5893c086502ead542f2e66708270e
Arthur Raff <arthur.raff@icloud.com>
2023-09-15 15:44:05 +0300

    FeedVC refactored

--- Cards App/WorkFlow/Feed/View/FeedView.swift
+++ Cards App/WorkFlow/Feed/View/FeedView.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 final class FeedView: UIView {
-    weak var delegate: FeedViewDelegate?
-    
     private lazy var contentView: UIView = {
         let view = UIView()
         
@@ -23,10 +21,9 @@ final class FeedView: UIView {
         tv.separatorColor = UIColor.systemIndigo.withAlphaComponent(0.4)
         tv.separatorInset = UIEdgeInsets(top: 0, left: 32, bottom: 0, right: 32)
         tv.showsVerticalScrollIndicator = true
-        tv.register(FeedTableViewCell.self, forCellReuseIdentifier: String(describing: FeedTableViewCell.self))
-        tv.dataSource = self
-        tv.delegate = self
-                
+        tv.register(FeedTableViewCell.self,
+                    forCellReuseIdentifier: String(describing: FeedTableViewCell.self))
+        
         return tv
     }()
     
@@ -41,67 +38,6 @@ final class FeedView: UIView {
     }
 }
 
-extension FeedView: UITableViewDataSource {
-    func tableView(_ tableView: UITableView,
-                   didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath,
-                              animated: true)
-        
-        delegate?.showFullCardDetailsScreenBy(index: indexPath.item)
-    }
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    func tableView(_ tableView: UITableView,
-                   numberOfRowsInSection section: Int) -> Int {
-        return CardsStore.shared.cards.count
-    }
-
-    func tableView(_ tableView: UITableView,
-                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let identifier = String(describing: FeedTableViewCell.self)
-        let cell = tableView.dequeueReusableCell(withIdentifier: identifier,
-                                                 for: indexPath) as? FeedTableViewCell
-
-        let card = CardsStore.shared.cards[indexPath.item]
-        
-        guard let cell = cell else {
-            return UITableViewCell()
-        }
-        
-        cell.configure(card: card)
-
-        return cell
-    }
-}
-
-extension FeedView: UITableViewDelegate {
-    func tableView(_ tableView: UITableView,
-                   heightForRowAt indexPath: IndexPath) -> CGFloat {
-        
-        return UITableView.automaticDimension
-    }
-
-    func tableView(_ tableView: UITableView,
-                   trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        
-        let trashAction = UIContextualAction(style: .destructive,
-                                             title: "Удалить") { [self] action, view, success in
-            
-            delegate?.removeCardBy(index: indexPath.item)
-            
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            tableView.reloadRows(at: [indexPath], with: .fade)
-            
-            success(true)
-        }
-        
-        return UISwipeActionsConfiguration(actions: [trashAction])
-    }
-}
-
 private extension FeedView {
     func setupScreen() {
         setupLayout()
@@ -111,12 +47,14 @@ private extension FeedView {
         add(subviews: [contentView,
                        tableView])
         
-        contentView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview {
+                $0.safeAreaLayoutGuide
+            }
         }
         
-        tableView.snp.makeConstraints { make in
-            make.edges.equalTo(contentView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalTo(contentView)
         }
     }
 }
@@ -124,5 +62,11 @@ private extension FeedView {
 extension FeedView {
     func reloadTableViewData() {
         tableView.reloadData()
+    }
+    
+    func setupTableView(dataSource: UITableViewDataSource,
+                        delegate: UITableViewDelegate) {
+        tableView.dataSource = dataSource
+        tableView.delegate = delegate
     }
 }
